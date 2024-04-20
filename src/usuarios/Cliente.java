b99449b58a8b9edcff1668cc12d16203ac6de056
Tomas Emilio Velasquez Delgado <tomvelcas@gmail.com>
2024-04-20 14:46:15 -0500

    id actualizado y mapas

--- src/usuarios/Cliente.java
+++ src/usuarios/Cliente.java
@@ -1,20 +1,14 @@
 package usuarios;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class Cliente extends Usuario {
     private String nombre;
     private String telefono;
-    private int id;
-    private static Map<Integer, Cliente> mapaClientes = new HashMap<>();
 
     public Cliente(String login, String password, String nombre, String telefono) {
         super(login, password);
         this.nombre = nombre;
         this.telefono = telefono;
-        this.id = obtenerNuevoId();
-        mapaClientes.put(id, this);
     }
 
     public String getNombre() {
@@ -25,10 +19,6 @@ public class Cliente extends Usuario {
         return telefono;
     }
 
-    public int getId() {
-        return id;
-    }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
@@ -36,6 +26,4 @@ public class Cliente extends Usuario {
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
-
-
 }
--- src/usuarios/ControladorUsuarios.java
+++ src/usuarios/ControladorUsuarios.java
@@ -1,66 +1,40 @@
 package usuarios;
+
 import java.util.Map;
+import java.util.HashMap;
 
 public class ControladorUsuarios {
-    private Map<String, Empleado> empleados;
-    private Map<String, Cliente> clientes;
+    private static Map<Integer, Empleado> mapaEmpleados = new HashMap<>();
+    private static Map<Integer, Cliente> mapaClientes = new HashMap<>();
 
     public OperadorSubasta crearOperador(String login, String password, String rol){
         OperadorSubasta operador = new OperadorSubasta(login, password, rol);
-        empleados.put(login, operador);
+        mapaEmpleados.put(Usuario.obtenerNuevoId(), operador);
         return operador;
-        }
+    }
     
     public Cajero crearCajero(String login, String password, String rol){
         Cajero cajero = new Cajero(login, password, rol);
-        empleados.put(login, cajero);
+        mapaEmpleados.put(Usuario.obtenerNuevoId(), cajero);
         return cajero;
-        }
+    }
 
     public AdministradorGaleria crearAdmin(String login, String password, String rol){
         AdministradorGaleria administrador = new AdministradorGaleria(login, password, rol);
-        empleados.put(login, administrador);
+        mapaEmpleados.put(Usuario.obtenerNuevoId(), administrador);
         return administrador;
-        }
+    }
 
     public Comprador crearComprador(String login, String password, String nombre, String telefono, int limiteCompras){
         Comprador comprador = new Comprador(login, password, nombre, telefono, limiteCompras);
-        clientes.put(login, comprador);
+        mapaClientes.put(Usuario.obtenerNuevoId(), comprador);
         return comprador;
-        }
+    }
 
     public Cliente crearCliente(String login, String password, String nombre, String telefono){
         Cliente cliente = new Cliente(login, password, nombre, telefono);
-        clientes.put(login, cliente);
+        mapaClientes.put(Usuario.obtenerNuevoId(), cliente);
         return cliente;
-        }
-
-    public Empleado obtenerEmpleado(String login){
-        return empleados.get(login);
-        }
-    public Cliente obtenerCliente(String login){
-        return clientes.get(login);
-        }
-
-    public void eliminarEmpleado(String login){
-        empleados.remove(login);
-        }
-    public void eliminarCliente(String login){
-        clientes.remove(login);
-        }
-    public void modificarEmpleado(String login, String password, String rol){
-        Empleado empleado = empleados.get(login);
-        empleado.setPassword(password);
-        empleado.setRol(rol);
-        }
-    public void modificarCliente(String login, String password, String nombre, String telefono){
-        Cliente cliente = clientes.get(login);
-        cliente.setPassword(password);
-        cliente.setNombre(nombre);
-        cliente.setTelefono(telefono);
-        }
-
-
-
+    }
 
 }
