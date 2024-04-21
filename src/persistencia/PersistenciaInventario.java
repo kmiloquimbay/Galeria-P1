dad36739957d27a5f7c0fa16dead3c49dd4e7fe6
kmiloquimbay <d.quimbay@uniandes.edu.co>
2024-04-21 15:08:02 -0500

    persistencia inventario terminado

--- src/persistencia/PersistenciaInventario.java
+++ src/persistencia/PersistenciaInventario.java
@@ -1,8 +1,14 @@
 package persistencia;
+
+import java.io.File;
 import java.io.PrintWriter;
+import java.nio.file.Files;
 
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import galeria.inventarioYpiezas.*;
+
+import java.util.List;
 
 public class PersistenciaInventario {
     
@@ -69,6 +75,14 @@ public class PersistenciaInventario {
             Video video = (Video) pieza;
             piezaJson.put("duracion", video.getDuracion());
             piezaJson.put("tamanio", video.getTamanio());
+        } else if (pieza.getTipoPieza().equals("Escultura")) {
+            Escultura escultura = (Escultura) pieza;
+            piezaJson.put("alto", escultura.getAlto());
+            piezaJson.put("ancho", escultura.getAncho());
+            piezaJson.put("material", escultura.getMaterialesConstruccion());
+            piezaJson.put("peso", escultura.getPeso());
+            piezaJson.put("profundidad", escultura.getProfundidad());
+            piezaJson.put("necesitaElectricidad", escultura.isNecesitaElectricidad());
         }
 
 
@@ -83,6 +97,69 @@ public class PersistenciaInventario {
         return autorJson;
     }
 
-    public cargarInventario(JSON)
+    public void cargarInventario(Inventario inventario){
+        String jsonCompleto = new String(Files.readAllBytes(new File("inventario.json").toPath()));
+        JSONObject inventarioJson = new JSONObject(jsonCompleto);
+
+        JSONArray piezasEnBodega = inventarioJson.getJSONArray("piezasEnBodega");
+        JSONArray piezasEnExhibicion = inventarioJson.getJSONArray("piezasEnExhibicion");
+        JSONArray piezasPasadas = inventarioJson.getJSONArray("piezasPasadas");
+
+        for (JSONObject piezaJson : piezasEnBodega) {
+            Pieza pieza = cargarPieza(piezaJson);
+            inventario.guardarEnBodega(pieza);
+        }
+
+        for (JSONObject piezaJson : piezasEnExhibicion) {
+            Pieza pieza = cargarPieza(piezaJson);
+            inventario.pasarAExhibicion(pieza);
+        }  
+
+        for (JSONObject piezaJson : piezasPasadas) {
+            Pieza pieza = cargarPieza(piezaJson);
+            inventario.pasarAPasadas(pieza);
+        }
+    }
+
+    public Pieza cargarPieza(JSONObject piezaJson){
+        String titulo = piezaJson.getString("titulo");
+        int anioCreacion = piezaJson.getInt("anioCreacion");
+        String lugarCreacion = piezaJson.getString("lugarCreacion");
+        String fechaDevolucion = piezaJson.getString("fechaDevolucion");
+        boolean disponibleVentaValorFijo = piezaJson.getBoolean("disponibleVentaValorFijo");
+        boolean bloqueada = piezaJson.getBoolean("bloqueada");
+        int precioFijo = piezaJson.getInt("precioFijo");
+        JSONArray autoresJson = piezaJson.getJSONArray("autores");
+        List<Autor> autores = new ArrayList<>();
+        for (JSONObject autorJson : autoresJson) {
+            Autor autor = cargarAutor(autorJson);
+            autores.add(autor);
+        }
+        String tipoPieza = piezaJson.getString("tipoPieza");
+
+        if (tipoPieza.equals("Pintura")) {
+            int alto = piezaJson.getInt("alto");
+            int ancho = piezaJson.getInt("ancho");
+            String tecnica = piezaJson.getString("tecnica");
+            return new Pintura(titulo, anioCreacion, lugarCreacion, fechaDevolucion, disponibleVentaValorFijo, bloqueada, alto, ancho, tecnica);
+        } else if (tipoPieza.equals("Fotografia")) {
+            String resolucion = piezaJson.getString("resolucion");
+            String tamanio = piezaJson.getString("tamanio");
+            return new Fotografia(titulo, anioCreacion, lugarCreacion, fechaDevolucion, disponibleVentaValorFijo, bloqueada, resolucion, tamanio);
+        } else if (tipoPieza.equals("Video")) {
+            String duracion = piezaJson.getString("duracion");
+            String tamanio = piezaJson.getString("tamanio");
+            return new Video(titulo, anioCreacion, lugarCreacion, fechaDevolucion, disponibleVentaValorFijo, bloqueada, duracion, tamanio);
+        } else if (tipoPieza.equals("Escultura")) {
+            int alto = piezaJson.getInt("alto");
+            int ancho = piezaJson.getInt("ancho");
+            String material = piezaJson.getString("material");
+            int peso = piezaJson.getInt("peso");
+            int profundidad = piezaJson.getInt("profundidad");
+            boolean necesitaElectricidad = piezaJson.getBoolean("necesitaElectricidad");
+            return new Escultura(titulo, anioCreacion, lugarCreacion, fechaDevolucion, disponibleVentaValorFijo, bloqueada, alto, ancho, profundidad, peso, material, necesitaElectricidad);
+        }
+    }
+
 
 }
