867b94b238999f1986e3ce95b2907ce363e67596
Tomas Emilio Velasquez Delgado <tomvelcas@gmail.com>
2024-04-20 22:39:30 -0500

    cajero

--- src/usuarios/Cajero.java
+++ src/usuarios/Cajero.java
@@ -1,6 +1,8 @@
 package usuarios;
 
 import galeria.Galeria;
+import galeria.compraYsubasta.Compra;
+import galeria.inventarioYpiezas.Pieza;
 
 public class Cajero extends Empleado{
     private Galeria galeria;
@@ -8,11 +10,21 @@ public class Cajero extends Empleado{
         super(login, password, rol,id);
         this.galeria=galeria;
     }
-    public void registrarPago(){
-        // Registra un pago
+    public String registrarPago(Compra compra, Pieza pieza, String idComprador){
+        this.galeria.getAdministrador().confirmarVenta(compra, pieza, idComprador);
+        return "Pago registrado";
     }
-    public void entregarPieza(){
+    public void entregarPieza(Pieza pieza, String idComprador){
         // Entrega una pieza
+        if (this.galeria.getAdministrador().verificarComprador(idComprador)){
+            this.galeria.getInventario().getPiezasEnBodega().remove(pieza);
+            this.galeria.getInventario().getPiezasEnExhibicion().add(pieza);
+            String nombre = pieza.getTitulo();
+            this.galeria.getAdministrador().desbloquearPieza(nombre);
+            //agregar a misPiezasActuales de propietario
+            this.galeria.getControladorUsuarios().obtenerPropietario(idComprador).getMisPiezasActuales().add(pieza);
+        }
+
     }
     
 }
